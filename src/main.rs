412c4be627a8897846d6a289870fe00326ddda60
Bede Kelly <bede@attio.com>
2023-03-09 23:48:31 +0000

    Basic HTML/CSS with aria labels for CPU meters

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::{
     net::SocketAddr,
     sync::{Arc, Mutex},
-    time::SystemTime,
+    time::{SystemTime},
 };
 
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
+use axum::{extract::State, http::StatusCode, routing::get, Json, Router, response::{IntoResponse, Html}};
 
-use serde::{Serialize};
+
 use sysinfo::{CpuExt, System, SystemExt};
 use tracing::Level;
 
+
 #[tokio::main]
 async fn main() {
+    let app_state = AppState::default();
+
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(Level::DEBUG)
         .finish();
@@ -20,12 +23,24 @@ async fn main() {
         .expect("Setting default global subscriber failed");
 
     let router = Router::new()
-        // .route("/", get(root_get))
+        .route("/", get(root_get))
         .route("/api/timestamp", get(api_timestamp_get))
         .route("/api/cpus", get(api_cpus_get))
-        .with_state(AppState {
-            sys: Arc::new(Mutex::new(System::new())),
-        });
+        .with_state(app_state.clone());
+
+
+    // Update CPU in the background.
+    tokio::task::spawn_blocking(move || {
+        let mut sys = System::new();
+        loop {
+            sys.refresh_cpu();
+            let v: Vec<f32> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+            let mut cpus = app_state.cpus.lock().unwrap();
+            *cpus = v;
+
+            std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+        }
+    });
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
@@ -52,23 +67,19 @@ async fn api_timestamp_get() -> String {
         .to_string()
 }
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    cpus: Arc<Mutex<Vec<f32>>>
 }
 
-#[derive(Serialize)]
-struct InfoResponse {
-    cpu_usage: Vec<f32>,
-}
-
-#[axum::debug_handler]
 async fn api_cpus_get(State(state): State<AppState>) -> (StatusCode, Json<Vec<f32>>) {
-    let mut sys = state.sys.lock().unwrap();
+    let cpus = state.cpus.lock().unwrap().clone();
 
-    sys.refresh_cpu();
+    (StatusCode::OK, Json(cpus))
+}
 
-    let response: Vec<f32> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
 
-    (StatusCode::OK, Json(response))
+    Html(markup)
 }
